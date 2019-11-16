47cea5988452f568b61c01c83be44cc016a5bcf5
Enzo Innocenzi <enzo.inno@gmail.com>
2019-11-16 16:01:53 +0100

    Updated script behavior

--- 4shack-jvc.user.js
+++ 4shack-jvc.user.js
@@ -1,58 +1,62 @@
 // ==UserScript==
 // @name         Remplacement de Noelshack par 4shack
 // @namespace    https://github.com/4sucres/4shack-jvc/
-// @author		 4sucres.org
-// @version      1.0
-// @match        http://www.jeuxvideo.com/forums/*
-// @match        http://m.jeuxvideo.com/forums/*
-// @match        http://m.jeuxvideo.com/forums/*
-// @match        http://www.jeuxvideo.com/messages-prives/*
+// @author		   4shack.org
+// @version      1.1
+// @match        *://*/*
+// @match        *://noelshack.com/*
+// @match        *://*.noelshack.com/*
+// @exclude      *://jeuxvideo.com/forums/*
+// @exclude      *://*.jeuxvideo.com/forums/*
+// @exclude      *://*.jeuxvideo.com/messages-prives/*
 // ==/UserScript==
 
 (function() {
   'use strict';
 
-  const debug = false;
-  const safe = false;
+  const debug = true;
+  const regex = /https?:\/\/image\.noelshack\.com\/fichiers\/((?:\w+\/){2,3}(?:(?:\d+-?){1,})((?:\w+-?){1,})(?:.([0-9a-z]+)))/gm;
+  const map = {
+    img: ['src', 'title', 'data-original-title'],
+    a: ['href', 'title', 'data-original-title'],
+  };
 
-  function exists(url) {
-      if (!safe) return true;
+  function apply(element, attributes = []) {
+    Array.prototype.forEach.call(attributes, attribute => {
+      document.querySelectorAll(`${element}[${attribute}]`).forEach(element => {
+        const match = regex.exec(element[attribute]);
 
-      try {
-          var http = new XMLHttpRequest();
-          http.open('HEAD', url, false);
-          http.send();
-      return 404 !== http.status;
-      } catch (e) {
-          return false;
-      }
-  }
+        if (debug) {
+          console.log(`Trying on a '${element}[${attribute}]' selector.`);
+        }
 
-  document.querySelectorAll('.bloc-contenu').forEach((el) => {
-      const content = el.innerHTML;
-      const regex = /<a href="(https?:\/\/image\.noelshack\.com\/fichiers\/((?:\w+\/){2,3}(?:(?:\d+-?){1,})((?:\w+-?){1,})(?:.([0-9a-z]+))))".*?>.+?<\/a>/gm;
-      const match = regex.exec(content);
-
-      if (match) {
-          const deprecated = match[1]; // Old URL
-          const source = `https://4shack.org/${match[2]}`; // New URL
-          const page = source; // TODO ; 4shack's actual image page
-          const filename = match[3]; // File name
-          const replacement = `
-            <a href="${page}" target="_blank">
-              <img class="img-shack" width="68" height="51"
-                src="${source}"
-                alt="${filename}">
-            </a>
-          `;
+        if (match) {
+          const deprecated = match[0]; // Old URL
+          const source = `https://4shack.org/${match[1]}`; // New URL
+          const filename = match[2]; // File name
 
           if (debug) {
-              console.log(`Using ${source} instead of ${deprecated}. File name is ${filename}.`);
+            console.log(`Using ${source} instead of ${deprecated} on '${element}[${attribute}].'`);
           }
 
-          if (exists(source)) {
-             el.innerHTML = content.replace(match[0], replacement);
-          }
+          element[attribute] = element[attribute].replace(deprecated, source);
+        }
+      });
+    });
+  }
+
+  function execute() {
+    Object.keys(map).map(element => {
+      const attributes = map[element];
+
+      if (debug) {
+        console.log(`Looking for '${element}' (trying ${attributes.length} attributes)`);
       }
-  });
+
+      apply(element, attributes);
+    });
+  }
+
+  execute(); // applies on load
+  setTimeout(execute, 1000); // applies a second time after everything is loaded
 })();
